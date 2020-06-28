538dbeecd2c86e5aca7387a5b2809b1d146a5ed8
xavi <xcampos@develoop.net>
2020-06-28 16:54:02 +0200

    [ADD] factura sin layout

---
+++ custom_modules/account_invoicing_custom/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_invoicing_custom",
+
+    'summary': """
+        Customizacion reports facturación""",
+
+    'description': """
+        Customizacion reports facturación
+    """,
+
+    'author': "Develoop Software",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'reports/custom_invoice_report.xml',
+        'reports/custom_invoice.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
