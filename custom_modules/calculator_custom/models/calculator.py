ee6d26df0e332d3249a468225ee1840d6891ff9f
Andres <acastillo@develoop.net>
2020-07-09 12:45:30 -0400

    modificacion de direcion de envio

--- custom_modules/calculator_custom/models/calculator.py
+++ custom_modules/calculator_custom/models/calculator.py
@@ -256,6 +256,10 @@ class calculator_custom_0(models.Model):
 
             if(client != _record_vals['partner_id']):
                 _record_vals['partner_id'] = client
+                client_data = self.env['res.partner'].search([('id','=',client)], limit=1)
+				if(client_data):
+	                _record_vals['partner_invoice_id'] = client_data.partner_invoice_id
+                
 
                 if _record_vals.get('name', _('New')) == _('New'):
                     seq_date = None
