46fc506b8b5875a90dd65f3a16b5be03f7b1c4a4
Andres <acastillo@develoop.net>
2020-09-29 13:00:23 -0400

    Signed-off-by: Andres <acastillo@develoop.net>

--- custom_modules/account_custom/models/account_custom.py
+++ custom_modules/account_custom/models/account_custom.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from itertools import groupby
-
 from odoo import api, fields, models, exceptions, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools import float_is_zero, float_compare
