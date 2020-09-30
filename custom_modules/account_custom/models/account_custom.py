0ae0f4baf055d270efb5dbfbde2640303e4f0f2b
Andres <acastillo@develoop.net>
2020-09-30 15:15:26 -0400

    Update

--- custom_modules/account_custom/models/account_custom.py
+++ custom_modules/account_custom/models/account_custom.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+import logging
 from odoo import api, fields, models, exceptions, SUPERUSER_ID, _
 from odoo.exceptions import AccessError, UserError, ValidationError
 from odoo.tools import float_is_zero, float_compare
-
+_logger = logging.getLogger(__name__)
 
 class AccountMoveCustom0(models.Model):
 
     _inherit = 'account.move'
 
-    x_importe_comision = fields.Monetary(compute='get_importe_comision', store=True)
+    x_importe_comision = fields.Monetary(compute='get_importe_comision')
 
     def get_importe_comision(self):
         for rec in self:
