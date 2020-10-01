957a6a8651d147d7ec9c41aad572615d577802e7
Andres <acastillo@develoop.net>
2020-10-01 17:54:05 -0400

    Update respuesta de Email

--- custom_modules/sale_mail_attachment_custom/models/sale_mail_attachment.py
+++ custom_modules/sale_mail_attachment_custom/models/sale_mail_attachment.py
@@ -14,6 +14,113 @@ class mail_custom_0(models.TransientModel):
 
     _inherit = 'mail.compose.message'
 
+
+    def get_mail_values(self, res_ids):
+        """Generate the values that will be used by send_mail to create mail_messages
+        or mail_mails. """
+        self.ensure_one()
+        results = dict.fromkeys(res_ids, False)
+        rendered_values = {}
+        mass_mail_mode = self.composition_mode == 'mass_mail'
+
+        # render all template-based value at once
+        if mass_mail_mode and self.model:
+            rendered_values = self.render_message(res_ids)
+        # compute alias-based reply-to in batch
+        reply_to_value = dict.fromkeys(res_ids, None)
+        if mass_mail_mode and not self.no_auto_thread:
+            records = self.env[self.model].browse(res_ids)
+            reply_to_value = self.env['mail.thread']._notify_get_reply_to_on_records(default=self.email_from, records=records)
+
+        blacklisted_rec_ids = []
+        if mass_mail_mode and issubclass(type(self.env[self.model]), self.pool['mail.thread.blacklist']):
+            BL_sudo = self.env['mail.blacklist'].sudo()
+            blacklist = set(BL_sudo.search([]).mapped('email'))
+            if blacklist:
+                targets = self.env[self.model].browse(res_ids).read(['email_normalized'])
+                # First extract email from recipient before comparing with blacklist
+                blacklisted_rec_ids.extend([target['id'] for target in targets
+                                            if target['email_normalized'] and target['email_normalized'] in blacklist])
+
+        for res_id in res_ids:
+            # static wizard (mail.message) values
+            
+            ##########################
+            reply_follower = ""
+            # ADD REPLY FOLLOWERS
+            if(self.model == 'sale.order'):
+                sale_order = self.env['sale.order'].search([('id','=', self.res_id)])
+                
+                emails_follower = []
+                if(sale_order.message_follower_ids):
+                    for partner in sale_order.message_follower_ids.partner_id:
+                        if(partner.email):
+                            emails_follower.append(partner.email)
+
+                if(emails_follower):
+                    reply_follower = ','.join(emails_follower)
+                    
+            ##########################
+
+            mail_values = {
+                'subject': self.subject,
+                'body': self.body or '',
+                'parent_id': self.parent_id and self.parent_id.id,
+                'partner_ids': [partner.id for partner in self.partner_ids],
+                'attachment_ids': [attach.id for attach in self.attachment_ids],
+                'author_id': self.author_id.id,
+                'email_from': self.email_from,
+                'record_name': self.record_name,
+                'no_auto_thread': self.no_auto_thread,
+                'mail_server_id': self.mail_server_id.id,
+                'mail_activity_type_id': self.mail_activity_type_id.id,
+                'reply_to': reply_follower ###########################
+            }
+
+            # mass mailing: rendering override wizard static values
+            if mass_mail_mode and self.model:
+                record = self.env[self.model].browse(res_id)
+                mail_values['headers'] = record._notify_email_headers()
+                # keep a copy unless specifically requested, reset record name (avoid browsing records)
+                mail_values.update(notification=not self.auto_delete_message, model=self.model, res_id=res_id, record_name=False)
+                # auto deletion of mail_mail
+                if self.auto_delete or self.template_id.auto_delete:
+                    mail_values['auto_delete'] = True
+                # rendered values using template
+                email_dict = rendered_values[res_id]
+                mail_values['partner_ids'] += email_dict.pop('partner_ids', [])
+                mail_values.update(email_dict)
+                if not self.no_auto_thread:
+                    mail_values.pop('reply_to')
+                    if reply_to_value.get(res_id):
+                        mail_values['reply_to'] = reply_to_value[res_id]
+                if self.no_auto_thread and not mail_values.get('reply_to'):
+                    mail_values['reply_to'] = mail_values['email_from']
+                # mail_mail values: body -> body_html, partner_ids -> recipient_ids
+                mail_values['body_html'] = mail_values.get('body', '')
+                mail_values['recipient_ids'] = [(4, id) for id in mail_values.pop('partner_ids', [])]
+
+                # process attachments: should not be encoded before being processed by message_post / mail_mail create
+                mail_values['attachments'] = [(name, base64.b64decode(enc_cont)) for name, enc_cont in email_dict.pop('attachments', list())]
+                attachment_ids = []
+                for attach_id in mail_values.pop('attachment_ids'):
+                    new_attach_id = self.env['ir.attachment'].browse(attach_id).copy({'res_model': self._name, 'res_id': self.id})
+                    attachment_ids.append(new_attach_id.id)
+                attachment_ids.reverse()
+                mail_values['attachment_ids'] = self.env['mail.thread']._message_post_process_attachments(
+                    mail_values.pop('attachments', []),
+                    attachment_ids,
+                    {'model': 'mail.message', 'res_id': 0}
+                )['attachment_ids']
+                # Filter out the blacklisted records by setting the mail state to cancel -> Used for Mass Mailing stats
+                if res_id in blacklisted_rec_ids:
+                    mail_values['state'] = 'cancel'
+                    # Do not post the mail into the recipient's chatter
+                    mail_values['notification'] = False
+
+            results[res_id] = mail_values
+        return results
+
     @api.onchange('template_id')
     def onchange_template_id_wrapper(self):
         self.ensure_one()
@@ -46,6 +153,7 @@ class mail_custom_0(models.TransientModel):
             ##########################
             if(model == 'sale.order'):
                 sale_order = self.env['sale.order'].search([('id','=', self.res_id)])
+                
                 if(sale_order and sale_order.x_studio_oportunidad):
                     crm_lead = self.env['crm.lead'].search([('id','=',sale_order.x_studio_oportunidad.id)])
                     if(crm_lead):
