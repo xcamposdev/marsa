f15c56babf9f683ee4d1e80bd69b68a3401c2e14
Andres <acastillo@develoop.net>
2020-11-11 08:18:08 -0400

    Costos de calculadora

---
+++ custom_modules/cost_calculation_custom/models/cost_calculation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import copy
+from itertools import groupby
+from decimal import Decimal
+from datetime import timedelta
+from odoo import api, fields, models, exceptions, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
+_logger = logging.getLogger(__name__)
+
+class cost_calculation_custom_0(models.Model):
+
+    _inherit = 'sale.order'
