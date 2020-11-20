90872c36809627f7956490630710188e009be923
Andres <acastillo@develoop.net>
2020-11-20 17:54:55 -0400

    Firma digital Marsa

---
+++ custom_modules/sign_pdf_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'sign_pdf_custom',
+    'version': '1.0.0.0',
+    'author': 'Develoop Software S.A.',
+    'category': 'Marsa',
+    'website': 'https://www.develoop.net/',
+    'depends': ['base','account'],
+    'summary': 'Sign PDF',
+    'description': """
+        Integracion de la firma digital
+        """,
+    'data': [
+        'views/sign_pdf_custom.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'demo': [],
+    'installable': True,
+}
