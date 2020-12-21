ab3763b804381d61f2171ea9613f4935de9e4057
Andres <acastillo@develoop.net>
2020-12-21 13:11:19 -0400

    Modificacion informe de costes

--- custom_modules/cost_calculation_report_custom/models/cost_calculation_report.py
+++ custom_modules/cost_calculation_report_custom/models/cost_calculation_report.py
@@ -22,58 +22,48 @@ class cost_calculation_report_custom(models.Model):
     def get_lines_custom(self):
         to_return = []
         for record in self:
+            oportunidad = ""
             if(record.origin):
-                origins = list(record.origin.split(","))
-                for origin in origins:
-                    sale_order = self.env['sale.order'].search([('name','=',origin)])
-                    if sale_order and sale_order.x_studio_oportunidad:
-                        if(sale_order.x_studio_medidor and sale_order.x_studio_montador and sale_order.x_studio_medidor == sale_order.x_studio_montador):
-                            self.addLine_custom(sale_order, "medidor", record, to_return)
-                        else:
-                            if sale_order.x_studio_medidor:
-                                self.addLine_custom(sale_order, "medidor", record, to_return)
-                            if sale_order.x_studio_montador:
-                                self.addLine_custom(sale_order, "montador", record, to_return)
-        
-        return to_return
+                sale_order = self.env['sale.order'].search([('name','=',record.origin)])
+                if sale_order and sale_order.x_studio_oportunidad:
+                    oportunidad = sale_order.x_studio_oportunidad.name
+                else:
+                    oportunidad = record.origin
+            else:
+                oportunidad = record.origin
 
-    def addLine_custom(self, sale_order, type, record, to_return):
-        if type == "medidor":
-            data_find = list(_data for _data in to_return if _data['partner_id'] == sale_order.x_studio_medidor.id)
-            contact = sale_order.x_studio_medidor
-        if type == "montador":
-            data_find = list(_data for _data in to_return if _data['partner_id'] == sale_order.x_studio_montador.id)
-            contact = sale_order.x_studio_montador
-        if not data_find:
-            data_find = {
-                'partner_id': contact.id,
-                'name': contact.name,
+            data_find = list(_data for _data in to_return if _data['partner_id'] == record.partner_id.id)
+            if not data_find:
+                data_find = {
+                'partner_id': record.partner_id.id,
+                'name': record.partner_id.name,
                 'total': 0,
-                'purchase': []
+                'purchase': [],
+                'group': []
+                }
+                to_return.append(data_find)
+            else: 
+                data_find = data_find[0]
+        
+            data_order = {
+                'purchase_date_order': record.date_order.strftime('%d/%m/%Y'),
+                'oportunidad': oportunidad,
+                'importe': record.amount_untaxed,
+                'purchase_order_line': []
             }
-            to_return.append(data_find)
-        else: 
-            data_find = data_find[0]
-        
-        data_order = {
-            'sale_date_order': sale_order.date_order.strftime('%d/%m/%Y'),
-            'purchase_date_order': record.date_order.strftime('%d/%m/%Y'),
-            'oportunidad': sale_order.x_studio_oportunidad.name if sale_order.x_studio_oportunidad else '',
-            'importe': record.amount_total,
-            'purchase_order_line': []
-        }
-        detail = []
-        for line in record.order_line:
-            if line.display_type == False:
-                detail.append({
-                    'product_id': line.product_id.id,
-                    'name': line.product_id.name,
-                    'quantity': line.product_uom_qty,
-                    'price': line.price_unit,
-                    'price_subtotal': line.price_unit,
-                })
-        
-        data_order['purchase_order_line'] = detail
-        data_find['purchase'].append(data_order)
-        data_find['total'] = data_find['total'] + record.amount_total
-        
+            detail = []
+            for line in record.order_line:
+                if line.display_type == False:
+                    detail.append({
+                        'product_id': line.product_id.id,
+                        'name': line.product_id.name,
+                        'quantity': line.product_uom_qty,
+                        'price': line.price_unit,
+                        'price_subtotal': line.price_subtotal,
+                    })
+            
+            data_order['purchase_order_line'] = detail
+            data_find['purchase'].append(data_order)
+            data_find['total'] = data_find['total'] + record.amount_untaxed
+
+        return to_return
