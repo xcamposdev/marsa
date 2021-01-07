07c41cbbd2262f5dbab42e3c22fb9a8020dd1939
Andres <acastillo@develoop.net>
2021-01-07 15:40:26 -0400

    Modificaciones el Reporte de Compras

---
+++ custom_modules/purchase_report_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Report Custom",
+    'version': '1.0.0.0',
+    'author': "Develoop Software",
+    'category': 'Uncategorized',
+    'summary': 'Modificación al reporte de Compras',
+    'website': "https://www.develoop.net/",
+    'description': """
+        - Modificación al reporte de Compras
+        """,
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_report_custom.xml'
+    ],
+    'demo': [],
+    "images": ['static/description/icon.png'],
+    "installable": True,
+    "auto_install": False,
+}
