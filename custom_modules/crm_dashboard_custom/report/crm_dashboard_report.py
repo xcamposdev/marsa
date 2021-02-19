92c9b1779cadd800209aba1f38f75de5def8f654
acastillo456 <acastillo@develoop.net>
2021-02-19 16:24:55 -0400

    Update Dashboard

--- custom_modules/crm_dashboard_custom/report/crm_dashboard_report.py
+++ custom_modules/crm_dashboard_custom/report/crm_dashboard_report.py
@@ -13,6 +13,7 @@ class crm_dashboard_report(models.Model):
     _rec_name = 'x_create_date'
     _order = 'x_create_date desc'
 
+    x_name = fields.Char(string="Oportunidad", readonly=True)
     x_create_date = fields.Date(string='Fecha de Creación', readonly=True)
     x_crm_quantity = fields.Integer(string="Ventas", readonly=True)
     x_measurements = fields.Integer(string="Mediciones", readonly=True)
@@ -20,6 +21,10 @@ class crm_dashboard_report(models.Model):
     x_mounting = fields.Integer(string='Montadas', readonly=True)
     x_finished = fields.Integer(string='Ventas Finalizadas', readonly=True)
     x_difference = fields.Integer(string='Diferencia', readonly=True)
+    x_montador = fields.Char(string="Montador", readonly=True)
+    x_montador_qty = fields.Char(string="Nº Reuniones Montador", readonly=True)
+    x_medidor = fields.Char(string="Medidor", readonly=True)
+    x_medidor_qty = fields.Char(string="Nº Reuniones Medidor", readonly=True)
 
     @api.model
     def _query(self):
@@ -31,26 +36,38 @@ class crm_dashboard_report(models.Model):
         op_finished_new = self.env['ir.config_parameter'].sudo().get_param('x_op_finished_new')
 
         return '''
-            SELECT ROW_NUMBER() OVER (order by x_crm_id) as id, x_create_date, 1 as x_crm_quantity,
+            SELECT ROW_NUMBER() OVER (order by x_crm_id) as id, x_name, x_create_date, 1 as x_crm_quantity,
                     CASE WHEN sum(x_measurements) > 0 THEN 1 ELSE 0 END as x_measurements,
                     CASE WHEN sum(x_production)>0 THEN 1 ELSE 0 END as x_production,
                     CASE WHEN sum(x_mounting)>0 THEN 1 ELSE 0 END as x_mounting,
                     CASE WHEN sum(x_finished)>0 THEN 1 ELSE 0 END as x_finished,
-                    CASE WHEN sum(x_finished)>0 THEN 0 ELSE 1 END as x_difference
+                    CASE WHEN sum(x_finished)>0 THEN 0 ELSE 1 END as x_difference,
+                    MIN(x_montador) as x_montador, MIN(x_montador_qty) as x_montador_qty, 
+                    MIN(x_medidor) as x_medidor, MIN(x_medidor_qty) as x_medidor_qty
             FROM (
-                SELECT crm.id as x_crm_id, crm.create_date as x_create_date, 1 as x_crm_quantity, 
-                    CASE WHEN mtv.old_value_char='%s' and mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_measurements,
-                    CASE WHEN mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_production,
-                    CASE WHEN mtv.old_value_char='%s' and mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_mounting,
-                    CASE WHEN mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_finished,
-                    --COUNT(*) FILTER (WHERE mtv.old_value_char='Medición' and mtv.new_value_char='Presupuesto teorico') as x_measurements,
-                    mtv.old_value_char, mtv.new_value_char
+                SELECT crm.id as x_crm_id, crm.name x_name, crm.create_date as x_create_date, 1 as x_crm_quantity, 
+                        CASE WHEN mtv.old_value_char='%s' and mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_measurements,
+                        CASE WHEN mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_production,
+                        CASE WHEN mtv.old_value_char='%s' and mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_mounting,
+                        CASE WHEN mtv.new_value_char='%s' THEN 1 ELSE 0 END as x_finished,
+                        mtv.old_value_char, mtv.new_value_char,
+                        reunion.montador as x_montador, reunion.montador_qty as x_montador_qty,
+                        reunion.medidor as x_medidor, reunion.medidor_qty as x_medidor_qty
                     
                 FROM crm_lead crm INNER JOIN mail_message mm ON crm.id=mm.res_id and mm.model='crm.lead'
-                                INNER JOIN mail_tracking_value mtv ON mm.id=mtv.mail_message_id and mtv.field='stage_id'
-                WHERE crm.active=true
-                ) a 
-            GROUP BY x_crm_id, x_create_date
+                                LEFT JOIN mail_tracking_value mtv ON mm.id=mtv.mail_message_id and mtv.field='stage_id'
+                                LEFT JOIN (SELECT ce.opportunity_id, 
+                                        MIN(r_p.name) FILTER (WHERE x_studio_subtipo='Montador') montador,
+                                        COUNT(r_p.name) FILTER (WHERE x_studio_subtipo='Montador') montador_qty,
+                                        MIN(r_p.name) FILTER (WHERE x_studio_subtipo='Medidor') medidor,
+                                        COUNT(r_p.name) FILTER (WHERE x_studio_subtipo='Medidor') medidor_qty
+                                FROM calendar_event ce INNER JOIN calendar_event_res_partner_rel ce_rel ON ce.id=ce_rel.calendar_event_id
+                                                INNER JOIN res_partner r_p ON r_p.id=ce_rel.res_partner_id
+                                                INNER JOIN res_users r_u ON r_u.partner_id=r_p.id and (r_u.x_studio_subtipo='Medidor' or r_u.x_studio_subtipo='Montador')
+                                Group by ce.opportunity_id) reunion ON reunion.opportunity_id=crm.id
+            WHERE crm.active=true
+            ) a 
+            GROUP BY x_crm_id, x_name, x_create_date
         ''' % (op_measurement_old, op_measurement_new, op_production_new, op_mounting_old, op_mounting_new, op_finished_new)
 
   
