a53c7e9fb6c1c72ca92d3b42c6922016c5c1303e
acastillo456 <acastillo@develoop.net>
2021-03-05 12:26:34 -0400

    Modulo de agrupacion OTE

---
+++ custom_modules/stock_rule_mrp_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "stock_rule_mrp_custom",
+    'version': '0.1',
+    'author': "Develoop Software",
+    'category': 'Uncategorized',
+    'summary': 'Modificar funcionalidad de stock en fabricación',
+    'website': "https://www.develoop.net/",
+    'description': """
+        Modificar funcionalidad de stock en fabricación
+        """,
+    'depends': ['mrp'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+}
